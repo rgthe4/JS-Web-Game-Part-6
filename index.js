91cbb88a01a821452f9248400379fe8889b54fc6
rgthe4 <102190136+rgthe4@users.noreply.github.com>
2022-06-20 14:21:09 -0500

    changes

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const pc = newPlayableCharacter(100, 110)
 
 // Create a non-playable character
 const npc = newNonPlayableCharacter(50, 300)
+npc.walkEast()             
+npc.stop()
+npc.walkEast()
 
 // have the NPC start walking east immediately
 npc.walkEast()
@@ -20,3 +23,25 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+npc.walkNorth(1400, () => {
+    npc.walkEast(1200, () => {
+        npc.walkSouth(300, () => {
+            npc.walkEast(1500, () => {
+                npc.walkSouth(1500, () => {
+                    npc.walkWest(2700, () => {
+                        npc.walkNorth(400)
+                    })
+                })
+            })
+        })
+    })
+})
+
+npc.walkNorth(1400)
+    .then(() => npc.walkEast(1200))
+    .then(() => npc.walkSouth(300))
+    .then(() => npc.walkEast(1500))
+    .then(() => npc.walkSouth(1500))
+    .then(() => npc.walkWest(2700))
+    .then(() => npc.walkNorth(400))
